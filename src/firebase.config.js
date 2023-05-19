6f257de0ccfdc007177a544d0fe2851b5271f7e6
ssjain13 <sonalsantoshjain@gmail.com>
2023-05-19 19:07:04 +0530

    Updating serviceaccount

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,6 +1,6 @@
 import * as firebaseClient from "firebase/app";
 import "firebase/auth";
-import serviceAccount from "../../serviceAccountKey.js";
+
 import admin from "firebase-admin";
 import dotenv from "dotenv";
 import { getAuth } from "firebase/auth";
@@ -21,6 +21,17 @@ export const client = firebaseClient.initializeApp(firebaseConfig);
 export const auth = getAuth(client);
 
 export const app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: "service_account",
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.private_key,
+    private_key: process.env.private_key,
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+  }),
   databaseURL: process.env.DATABASE_URL,
 });
